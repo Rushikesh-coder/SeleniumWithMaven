aeb331f023f26ee06e28a31ff9456354ba148551
Admin <Admin@DESKTOP-RKOMUFV>
2021-08-17 10:28:36 +0530

    day 12

--- SeleniumWithMaven/src/test/java/com/day10/RedioButton/com/SeleniumRedioButtonPage.java
+++ SeleniumWithMaven/src/test/java/com/day10/RedioButton/com/SeleniumRedioButtonPage.java
@@ -30,11 +30,6 @@ public class SeleniumRedioButtonPage {
 	}
 	
 	
-	//Added to check git working....................
-//	public void check()
-//	{
-//		Assert.assertFalse(this.checkIsRadioButtonIsSelected(strPath));
-//	}
 	
 	
 	public void clickonGetCheckedValueButton()     //Click on get Value button
